35503fc31955a5772b6629b5accc0e78cc4d4b20
blast66 <113650618+blast66@users.noreply.github.com>
2022-09-23 11:40:29 +0800

    Add files via upload

--- 新增資料夾/test.java
+++ 新增資料夾/test.java
@@ -2,7 +2,6 @@ public class test{
 	public static void main(String[] args){
 		System.out.println("cbra");
       		System.out.println("args[0] is "+args[0]);  
-      		System.out.println("args[0] is "+args[1]);  
      		System.out.println("args[0] is "+args[3]); 
 	}
 }
